a02725c91c199b1c083dc4679132937f63d4974f
kimondo88 <kimondo1988@o2.pl>
2022-04-23 15:58:37 +0200

    finish homepage, add few flex util class

--- src/feature/homepage.js
+++ src/feature/homepage.js
@@ -12,7 +12,7 @@ export default function Homepage() {
     },[])
 
     return (
-        <div className="">
+        <div className="flex flex-col flex-between">
             <nav className="flex flex-end my-3">
                 <ul className="primary-navigation underline-indicator flex flex-basis-60 bg-dark--alpha">
                     <li className="active"><Link className="txt-white ff-sans-cond letter-spacing-2 fs-300" to="/"><span className="mxr-1">00</span>Home</Link></li>
@@ -25,15 +25,17 @@ export default function Homepage() {
                 </ul>
             </nav>
 
-            <div className="flex">
-                <div className="block">
-                    <p className='txt-light ff-sans-cond uppercase fs-400 letter-spacing-2'>So, you want to travel to</p>
+            <div className="flex" style={{
+                "width" : "80%", "marginTop" : "15%", 
+                "marginInline" : "auto", "--flow-space" : "3rem"}}>
+                <div className="block flow flex-basis-100">
+                    <p className='txt-light ff-sans-cond uppercase letter-spacing-1 fs-400'>So, you want to travel to</p>
                     <p className='ff-serif uppercase fs-900'>Space</p>
-                    <p className='txt-light ff-sans-normal uppercase fs-200'>Let's face it; if you want to go to space, you might as well genuinely go to
+                    <p className='txt-light ff-sans-normal fs-400'>Let's face it; if you want to go to space, you might as well genuinely go to
                         outer space and not hover kind of on the edge of it. Well sit back, and relax
                         because we'll give you a truly out of this world experience!</p>
                 </div>
-                <div className="flex">
+                <div className="flex mxr-5 flex-als-end" style={{"height": "80%"}}>
                     <Link className="large-button anim bg-white txt-dark fs-600 ff-serif uppercase" to="/explore">Explore</Link>
                 </div>
             </div>
